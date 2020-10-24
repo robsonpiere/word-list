0b3b0510afd71f182b6f98b10676c2d05419d5f3
Robson Piere <robsonpiere@outlook.com>
2020-10-24 09:28:15 -0300

    Add cors

--- wordvector/ext/configuration.py
+++ wordvector/ext/configuration.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from wordvector.ext import handlers
+from wordvector.ext import handlers, cors
 
 def init_app(app: Flask) -> Flask:
     """
     Adiciona as configurações
     """
     handlers.init_app(app)
+    cors.init_app(app)
     return app
