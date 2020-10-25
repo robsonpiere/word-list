da4c2bc64ef68c98d87aec26a503b7303d1b7e09
Robson Piere <robsonpiere@outlook.com>
2020-10-25 11:27:48 -0300

    Add /vocabulary route

--- wordlist/blueprints/api/__init__.py
+++ wordlist/blueprints/api/__init__.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
-from . import home
+from . import home, vocabulary
 
 api_blueprint = Blueprint('api', __name__)
 
 home.add_rules(api_blueprint)
+vocabulary.add_rules(api_blueprint)
 
 
--- wordlist/tests/test_app.py
+++ wordlist/tests/test_app.py
@@ -15,3 +15,7 @@ class AppTestCase(unittest.TestCase):
     def test_status_code_home(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_status_not_found(self):
+        response = self.client.get('/dinheiro')
+        self.assertEqual(response.status_code, 404)
